a4c8281add96fcade1beece460988e999d0b1f08
Alberto Madrazo <otreblatercero@gmail.com>
2014-10-09 21:45:58 -0500

    Para no perder la práctica, subí unas vistas interesantes...

--- tango/tango_with_django_project/rango/views.py
+++ tango/tango_with_django_project/rango/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("Rango says hello world!")
+
+def follatela(request):
+	da_loop = 0
+	for i in range(4):
+		da_loop += i * 9 - 17
+
+	return HttpResponse(da_loop)
